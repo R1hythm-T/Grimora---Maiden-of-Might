44206638e1b50f2a5a8fc68974d88ffae0ad41bf
R1hythm-T <ruchirtiwari2003@gmail.com>
2025-05-04 19:33:59 +0530

    Fixed the merge
    till 108

---
+++ Assets/Scripts/Controllers/Skill Controllers/CloneSkillController.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneSkillController : MonoBehaviour
+{
+    private Player player;
+    private SpriteRenderer sr;
+    private Animator anim;
+    [SerializeField] private float colorLosingSpeed;
+
+    private float cloneTimer;
+    [SerializeField] private Transform attackCheck;
+    [SerializeField] private float attackCheckRadius = 1;
+    private int facingDir = 1;
+
+    private bool canDuplicateClone;
+    private float chanceToDuplicate;
+
+    [Space]
+    [SerializeField] private LayerMask whatIsEnemy;
+    [SerializeField] private float closestEnemyCheckRadius = 25;
+    [SerializeField] private Transform closestEnemy;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(FaceClosestTarget());
+    }
+
+    private void Update()
+    {
+        cloneTimer -= Time.deltaTime;
+
+        if (cloneTimer < 0 )
+        {
+            sr.color = new Color(1, 1, 1, sr.color.a - (Time.deltaTime * colorLosingSpeed));
+
+            if (sr.color.a <= 0)
+                Destroy(gameObject);
+        }
+    }
+
+    public void SetupClone(Transform _newTransform, float _cloneDuration, bool _canAttack, Vector3 _offset, bool _canDuplicate, float _chanceToDuplicate, Player _player)
+    {
+        if (_canAttack)
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+
+        player = _player;
+        transform.position = _newTransform.position + _offset;
+        cloneTimer = _cloneDuration;
+
+        canDuplicateClone = _canDuplicate;
+        chanceToDuplicate = _chanceToDuplicate;
+    }
+
+    private void AnimationTrigger()
+    {
+        cloneTimer = -.1f;
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCheck.position, attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                player.stats.DoDamage(hit.GetComponent<CharacterStats>());
+
+                if (canDuplicateClone)
+                {
+                    if (Random.Range(0, 100) < chanceToDuplicate)
+                    {
+                        SkillManager.instance.clone.CreateClone(hit.transform, new Vector3(.5f * facingDir, 0));
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator FaceClosestTarget()
+    {
+        yield return null;
+
+        FindClosestEnemy();
+
+        if (closestEnemy != null)
+        {
+            if (transform.position.x > closestEnemy.position.x)
+            {
+                facingDir = -1;
+                transform.Rotate(0, 180, 0);
+            }
+        }
+    }
+
+    private void FindClosestEnemy()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, closestEnemyCheckRadius, whatIsEnemy);
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var hit in colliders)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+
+            if (distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                closestEnemy = hit.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Skills/BlackholeSkill.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeSkill : Skill
+{
+    [SerializeField] private float cloneCooldown;
+    [SerializeField] private int amountOfAttacks;
+    [SerializeField] private float blackholeDuration;
+    [Space]
+    [SerializeField] private GameObject blackHolePrefab;
+    [SerializeField] private float maxSize;
+    [SerializeField] private float growSpeed;
+    [SerializeField] private float shrinkSpeed;
+
+    BlackholeSkillController currentBlackhole;
+    public override bool CanUseSkill()
+    {
+        return base.CanUseSkill();
+    }
+
+    public override void UseSkill()
+    {
+        base.UseSkill();
+
+        GameObject newBlackHole = Instantiate(blackHolePrefab, player.transform.position, Quaternion.identity);
+
+        currentBlackhole = newBlackHole.GetComponent<BlackholeSkillController>();
+        currentBlackhole.SetupBlackHole(maxSize, growSpeed, shrinkSpeed, amountOfAttacks, cloneCooldown, blackholeDuration);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public bool BlackholeFinished()
+    {
+        if (!currentBlackhole)
+            return false;
+
+        if (currentBlackhole.playerCanExitState)
+        {
+            currentBlackhole = null;
+            return true;
+        }
+
+        return false;
+    }
+
+    public float GetBlackHoleRadius()
+    {
+        return maxSize / 2;
+    }
+}
