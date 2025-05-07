ea3aa8f6086b710c70087454b9b50e1a45bc2f05
youdontknowme-15 <keshav.sharma150803@gmail.com>
2025-05-07 20:45:33 +0530

    Craft UI done
    (131)

--- Assets/Scripts/UI/UI_CraftSlot.cs
+++ Assets/Scripts/UI/UI_CraftSlot.cs
@@ -1,10 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UI_CraftSlot : UI_ItemSlot
 {
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public void SetupCraftSlot(ItemData_Equipment _data)
+    {
+        if (_data == null)
+        {
+            return;
+        }
+        item.data = _data;
+        itemImage.sprite = _data.itemIcon;
+        itemText.text = _data.itemName;
+
+        if (itemText.text.Length > 12)
+        {
+            itemText.fontSize = itemText.fontSize * .7f;
+        }
+        else
+        {
+            itemText.fontSize = 24;
+        }
+    }
     private void OnEnable()
     {
         UpdateSlot(item);
@@ -12,7 +34,6 @@ public class UI_CraftSlot : UI_ItemSlot
 
     public override void OnPointerDown(PointerEventData eventData)
     {
-        ItemData_Equipment craftData = item.data as ItemData_Equipment;
-        Inventory.instance.CanCraft(craftData, craftData.craftingMaterials);
+     ui.craftWindow.SetupCraftWindow(item.data as ItemData_Equipment);
     }
 }
--- Assets/Scripts/UI/UI_EquipmentSlot.cs
+++ Assets/Scripts/UI/UI_EquipmentSlot.cs
@@ -19,6 +19,9 @@ public class UI_EquipmentSlot : UI_ItemSlot
             return;
         Inventory.instance.UnequipItem(item.data as ItemData_Equipment);
         Inventory.instance.AddItem(item.data as ItemData_Equipment);
+        
+        ui.itemToolTip.HideToolTip();
+
         CleanUpSlot();
     }
 }
